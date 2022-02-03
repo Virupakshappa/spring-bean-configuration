a548fb2fde23df2f89f7f11a50860d0d7222396f
Veeru <vjanadri7@gmail.com>
2022-02-04 02:38:11 +0530

    JP-20: Updating the repo with @Qualifier annotation

--- springframe/src/main/java/com/janadri/springframe/phone/SnapDragon.java
+++ springframe/src/main/java/com/janadri/springframe/phone/SnapDragon.java
@@ -1,8 +1,10 @@
 package com.janadri.springframe.phone;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary           It will prioritize the component "SnapDragon" over "MediaTek"
 public class SnapDragon implements MobileProcessor {
 
 	public void process() {
